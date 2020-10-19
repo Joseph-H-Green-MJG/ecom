9b0b1c1757244f7e9e0c4461bb2b1d29cf7d5f8e
Joseph <thecaliman@gmail.com>
2020-10-19 10:33:01 -0700

    build out layout grid and header navbar components

---
+++ src/components/headernavbar/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class header extends Component {
+    render() {
+        return (
+            <div className='header'>
+                <img src='https:via.placeholder.com/50x50'/>
+            </div>
+        );
+    }
+}
+
+export default header;
